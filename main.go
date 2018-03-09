a1d56a1ed1fea4f083971dd473b2bc77b9581069
Tristan Rice <rice@fn.lc>
2018-03-09 15:51:12 -0800

    badger + NodeConfig

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"proj2_f5w9a_h6v9a_q7w9a_r8u8_w1c0b/server"
+	"proj2_f5w9a_h6v9a_q7w9a_r8u8_w1c0b/serverpb"
 )
 
 func main() {
@@ -13,7 +14,10 @@ func main() {
 }
 
 func run() error {
-	s, err := server.New()
+	s, err := server.New(serverpb.NodeConfig{
+		Path:     "tmp/node1",
+		MaxPeers: 10,
+	})
 	if err != nil {
 		return err
 	}
--- server/server.go
+++ server/server.go
@@ -5,8 +5,10 @@ import (
 	"log"
 	"net"
 	"os"
+	"path/filepath"
 	"proj2_f5w9a_h6v9a_q7w9a_r8u8_w1c0b/serverpb"
 
+	"github.com/dgraph-io/badger"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
@@ -17,18 +19,50 @@ var ErrUnimplemented = errors.New("unimplemented")
 type Server struct {
 	log        *log.Logger
 	grpcServer *grpc.Server
+	config     serverpb.NodeConfig
+	db         *badger.DB
+	l          net.Listener
 }
 
 // New returns a new server.
-func New() (*Server, error) {
+func New(c serverpb.NodeConfig) (*Server, error) {
 	s := &Server{
 		log:        log.New(os.Stderr, "", log.Flags()|log.Lshortfile),
 		grpcServer: grpc.NewServer(),
+		config:     c,
 	}
+
+	if len(c.Path) == 0 {
+		return nil, errors.Errorf("config: path must not be empty")
+	}
+	if err := os.MkdirAll(c.Path, 0700); err != nil {
+		return nil, err
+	}
+
+	badgerDir := filepath.Join(c.Path, "badger")
+	opts := badger.DefaultOptions
+	opts.Dir = badgerDir
+	opts.ValueDir = badgerDir
+	db, err := badger.Open(opts)
+	if err != nil {
+		return nil, err
+	}
+	s.db = db
 
 	serverpb.RegisterNodeServer(s.grpcServer, s)
 
 	return s, nil
+}
+
+func (s *Server) Close() error {
+	if err := s.db.Close(); err != nil {
+		return err
+	}
+	s.grpcServer.GracefulStop()
+	if err := s.l.Close(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *Server) Hello(ctx context.Context, req *serverpb.HelloRequest) (*serverpb.HelloResponse, error) {
@@ -41,6 +75,7 @@ func (s *Server) Listen(addr string) error {
 	if err != nil {
 		return err
 	}
+	s.l = l
 	s.log.Printf("Listening to %s", l.Addr().String())
 	return s.grpcServer.Serve(l)
 }
